3dcdc82455ac6644adce99c580a5472d556ec2ce
gness1804 <graham.nessler@gmail.com>
2017-01-21 15:31:14 -0700

    Refactor router js

--- router.js
+++ router.js
@@ -1,29 +1,26 @@
-var Router = function () {
+export const Router = () => {
   this.routes = [];
-}
+};
 
 Router.prototype.add = function (method, url, handler) {
   this.routes.push({
-    method: method,
-    url: url,
-    handler: handler,
+    method,
+    url,
+    handler,
   });
 };
 
 Router.prototype.resolve = function (request, response) {
-  var path = require('url').parse(request.url).pathname;
+  let path = require('url').parse(request.url).pathname;
 
-  return this.routes.some(function (route) {
-    var match = route.url.exec(path);
+  return this.routes.some((route) => {
+    let match = route.url.exec(path);
     if (!match || route.method !== request.method) {
       return false;
     }
 
-    var urlParts = match.slice(1).map(decodeURIComponent);
+    let urlParts = match.slice(1).map(decodeURIComponent);
     route.handler.apply(null, [request, response].concat(urlParts));
     return true;
   });
 };
-
-
-module.exports = Router;
